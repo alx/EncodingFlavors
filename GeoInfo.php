87e9d1411ce32bce5930bd84b23ca048f3122283
Alexandre Girard <git@alexgirard.com>
2014-09-09 18:15:51 +0200

    first commit

---
+++ GeoInfo.php
@@ -0,0 +1,26 @@
+<?php
+class EncodingFlavors {
+
+    static function Load() {
+    }
+
+    static function Info() {
+        return array (
+            'name'    => 'EncodingFlavors',
+            'author'  => 'Alexandre Girard',
+            'version' => '1.0.0',
+            'site'    => 'http://alexgirard.com/',
+            'notes'   => 'Add custom encoding flavors, with various options (watermark, player thumbnail preview, ...)'
+        );
+    }
+
+    static function Install() {
+    }
+
+    static function Uninstall() {
+    }
+
+    static function Settings() {
+    }
+}
+?>
---
+++ README.md
@@ -0,0 +1,10 @@
+CumulusClips Plugin : EncodingFlavors
+=====================================
+
+Add custom encoding flavors, with various options (watermark, player thumbnail preview, ...)
+
+# Install
+
+1. Go to your CumulusClips plugin folder
+2. ```git clone https://github.com/alx/EncodingFlavors.git```
+3. In CumulusClips admin interface, enable EncodingFlavors plugin from plugins panel
