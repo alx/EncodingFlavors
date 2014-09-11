29252b4ea308a298a9cbdada432b611129ca7d18
Alexandre Girard <git@alexgirard.com>
2014-09-11 18:29:17 +0200

    add encoding process in plugin

--- EncodingFlavors.php
+++ EncodingFlavors.php
@@ -2,16 +2,67 @@
 class EncodingFlavors {
 
     static function Load() {
+      Plugin::Attach ( 'encode.update' , array( __CLASS__ , 'StartEncodingFlavors' ) );
+    }
+
+    static function StartEncodingFlavors() {
+      global $video;
+
+      $db = Database::GetInstance();
+
+      // Set MySQL wait_timeout to 10 hours to prevent 'MySQL server has gone away' errors
+      $db->Query ("SET @@session.wait_timeout=36000");
+
+      $query = 'SELECT * FROM `'.DB_PREFIX.'encoding_flavors`';
+      $result = $db->Query ($query);
+
+      while ($encoding = $db->FetchObj ($result)):
+        EncodingFlavors::EncodeVideoWithFlavor($video, $flavor);
+      endwhile;
+    }
+
+    static function EncodeVideoWithFlavor($video, $flavor) {
+      App::LoadClass ('Video');
+      global $video, $config;
+
+      $raw_video = UPLOAD_PATH . '/temp/' . $video->filename . '.' . $video->original_extension;
+      $ffmpeg_path = Settings::Get ('ffmpeg');
+
+      $file = UPLOAD_PATH . '/' . $flavor->folder . '/' . $video->filename . '.' . $flavor->extension;
+
+      // Debug Log
+      $config->debug_conversion ? App::Log (CONVERSION_LOG, "\nPreparing for: Encoding flavor " . $flavor->name . "...") : null;
+
+      ### Encode raw video to FLV
+      $encoding_command = "$ffmpeg_path -i $raw_video " . $flavor->command_options . " $file >> $debug_log 2>&1";
+
+      // Debug Log
+      $log_msg = "\n\n\n\n==================================================================\n";
+      $log_msg .= "ENCODING FLAVOR : " . $flavor->name . "\n";
+      $log_msg .= "==================================================================\n\n";
+      $log_msg .= "Encoding Command: $encoding_command";
+      $log_msg .= "Encoding Output:\n\n";
+      $config->debug_conversion ? App::Log (CONVERSION_LOG, 'Encoding Flavor Command: ' . $encoding_command) : null;
+      App::Log ($debug_log, $log_msg);
+
+      ### Execute encoding command
+      exec ($encoding_command);
+
+      // Debug Log
+      $config->debug_conversion ? App::Log (CONVERSION_LOG, 'Verifying flavor video was created...') : null;
+
+      ### Verify video file was created successfully
+      if (!file_exists ($file) || filesize ($file) < 1024*5) throw new Exception ("The flavor file was not created. The id of the video is: $video->video_id");
     }
 
     static function Info() {
-        return array (
-            'name'    => 'EncodingFlavors',
-            'author'  => 'Alexandre Girard',
-            'version' => '1.0.0',
-            'site'    => 'http://alexgirard.com/',
-            'notes'   => 'Add custom encoding flavors, with various options (watermark, player thumbnail preview, ...)'
-        );
+      return array (
+        'name'    => 'EncodingFlavors',
+        'author'  => 'Alexandre Girard',
+        'version' => '1.0.0',
+        'site'    => 'http://alexgirard.com/',
+        'notes'   => 'Add custom encoding flavors, with various options (watermark, player thumbnail preview, ...)'
+      );
     }
 
     static function Install() {
@@ -20,6 +71,8 @@ class EncodingFlavors {
       $create_query = 'CREATE TABLE `'.DB_PREFIX.'encoding_flavors` (';
       $create_query .= '  `encoding_id` bigint(20) NOT NULL AUTO_INCREMENT,';
       $create_query .= '  `name` varchar(255) NOT NULL,';
+      $create_query .= '  `folder` varchar(255) NOT NULL,';
+      $create_query .= '  `extension` varchar(255) NOT NULL,';
       $create_query .= '  `command_options` text NOT NULL,';
       $create_query .= '  PRIMARY KEY (`encoding_id`)';
       $create_query .= ') DEFAULT CHARSET=utf8';
